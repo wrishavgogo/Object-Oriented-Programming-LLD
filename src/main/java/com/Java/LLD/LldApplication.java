f0e00a9bae9f85f6c2bf305745b1e833d9a053d3
Wrishav <wbhattacharyya@gmail.com>
2024-08-02 10:01:33 +0530

    initial commit

---
+++ src/main/java/com/Java/LLD/LldApplication.java
@@ -0,0 +1,13 @@
+package com.Java.LLD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LldApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Java/LLD/LldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Java.LLD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
